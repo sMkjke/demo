a8f388ae0363e695090ee321e03aac46505bdedf
Mikhail Voronov <smirk06@yandex.ru>
2022-06-16 22:54:36 +0300

    commit

--- xml_json/src/main/java/Main.java
+++ xml_json/src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.nio.charset.StandardCharsets;
 public class Main {
 
     public static void main(String[] args) {
+
         XPathExample xPathExample = new XPathExample();
 
         xPathExample.changeNode("//messagedate", "2022-06-16T22:00:00");
@@ -11,20 +12,11 @@ public class Main {
         xPathExample.changeNode("//name", "Boris");
         xPathExample.changeNode("//patronymic", "Nikolayevich");
         xPathExample.changeNode("//email", "freedomrussia@ru.ru");
-//        xPathExample.writeXml(XPathExample.CHANGED_MESSAGE);
-        xPathExample.saveXml(XPathExample.CHANGED_MESSAGE);
-//        xPathVal.changeNodeText("//messagedate", "2021-07-21T08:00:00");
-//        xPathVal.changeNodeText("//population", "111");
-//        xPathVal.changeNodeText("//surname", "Ivanov");
-//        xPathVal.changeNodeText("//name", "Ivan");
-//        xPathVal.changeNodeText("//patronymic", "Ivanovich");
-//        xPathVal.changeNodeText("//email", "ivanivanovich123@mail.ru");
-
-//        xPathVal.saveXml(PathVariables.MESSAGE_AFTER_CHANGE_XML_FULL_PATH);
+        xPathExample.changeNode("//department", "dit");
+        xPathExample.writeXml(XPathExample.CHANGED_MESSAGE);
 
         System.out.println(xPathExample.getSimpleValidator().validate(XPathExample.XSD_MESSAGE,XPathExample.CHANGED_MESSAGE));
-//        boolean answer = XsdValidator.validateXMLSchema(PathVariables.MESSAGE_XSD, PathVariables.MESSAGE_AFTER_CHANGE_XML_FULL_PATH);
-//        System.out.println("Результат валидации: " + answer);
+
     }
 
 }
--- xml_json/src/main/java/XPathExample.java
+++ xml_json/src/main/java/XPathExample.java
@@ -19,7 +19,7 @@ public class XPathExample {
     public static final String XML_MESSAGE = "Message.xml";
     public static final String XSD_MESSAGE = "Message.xsd";
     public static final String CHANGED_MESSAGE = "xml_json/src/main/resources/ChangedMessage.xml";
-    public static final String XSLT_FILENAME = "xml_json/src/main/resources/style.xslt";
+    public static final String XSLT_FILENAME = "xml_json/src/main/resources/xslt/style.xslt";
 
     private Document document;
     private SimpleValidator simpleValidator;
@@ -30,49 +30,12 @@ public class XPathExample {
     }
 
 
-    //    private Document setDocument() {
-//        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-//
-//        try (InputStream is = readXmlFileIntoInputStream(XML_MESSAGE)) {
-//
-//            // parse XML file
-//            DocumentBuilder db = dbf.newDocumentBuilder();
-//
-//            // read from a project's resources folder
-////            Document doc = db.parse(is);
-//            System.out.println("parsed!");
-//            return db.parse(is);
-//
-//        } catch (ParserConfigurationException | SAXException | IOException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println("null!");
-//        return null;
-//    }
-    private Document setDocument() {
-        // Загружаем файл из ресурсов как InputStream
-        ClassLoader classloader = XPathExample.class.getClassLoader();
-        var file = classloader.getResourceAsStream(XML_MESSAGE);
+    public void changeNode(final String xPath, final String value) {
+        XPathFactory pf = XPathFactory.newInstance();
+        XPath xpath = pf.newXPath();
 
-        DocumentBuilder documentBuilder = null;
         try {
-            documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            return document = documentBuilder.parse(file);
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("null");
-        return null;
-    }
-
-
-    public void changeNode(String xPath, String value) {
-        XPathFactory pathFactory = XPathFactory.newInstance();
-        XPath xpath = pathFactory.newXPath();
-
-        XPathExpression exp = null;
-        try {
-            exp = xpath.compile(xPath);
+            XPathExpression exp = xpath.compile(xPath);
             NodeList nodes = (NodeList) exp.evaluate(document, XPathConstants.NODESET);
             nodes.item(0).setTextContent(value);
         } catch (XPathExpressionException e) {
@@ -80,50 +43,45 @@ public class XPathExample {
         }
     }
 
-    //    public void writeXml(String path) {
-//        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-//        try {
-////            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-//
-//            // The default add many empty new line, not sure why?
-//            // https://mkyong.com/java/pretty-print-xml-with-java-dom-and-xslt/
-//            // Transformer transformer = transformerFactory.newTransformer();
-//
-//            // add a xslt to remove the extra newlines
-////            File file = new File(XSLT_FILENAME);
-////            Transformer transformer = transformerFactory.newTransformer(
-////                    new StreamSource(file));
-//                Transformer transformer = TransformerFactory.newInstance().newTransformer();
-//            // pretty print
-//            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-//            transformer.setOutputProperty(OutputKeys.STANDALONE, "no");
-//
-//            DOMSource source = new DOMSource(document);
-//            StreamResult result = new StreamResult(new File(path));
-//
-//            transformer.transform(source, result);
-//        } catch (TransformerException e) {
-//            e.printStackTrace();
-//        }
-//    }
-    public void saveXml(String path) {
-        Transformer transformer = null;
+    public void writeXml(final String path) {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
         try {
-            transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.transform(new DOMSource(document), new StreamResult(new File(CHANGED_MESSAGE)));
+            // add a xslt to remove the extra newlines
+            Transformer transformer = transformerFactory.newTransformer(
+                    new StreamSource(new File(XSLT_FILENAME)));
+
+            // pretty print
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.STANDALONE, "no");
+
+            transformer.transform(new DOMSource(document), new StreamResult(new File(path)));
+
         } catch (TransformerException e) {
             e.printStackTrace();
         }
     }
 
-    private InputStream readXmlFileIntoInputStream(final String fileName) {
-//        return XPath.class.getClassLoader().getResourceAsStream(fileName);
-        //working only if file in the same package
-        return this.getClass().getResourceAsStream(fileName);
+    private Document setDocument() {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try (InputStream is = readXmlFileIntoInputStream(XML_MESSAGE)) {
+
+            // parse XML file
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            // read from a project's resources folder
+            return db.parse(is);
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("null!");
+        return null;
     }
 
-    public Document getDocument() {
-        return document;
+    private InputStream readXmlFileIntoInputStream(final String fileName) {
+        //working only if file in the same package
+        return this.getClass().getResourceAsStream(fileName);
     }
 
     public SimpleValidator getSimpleValidator() {
